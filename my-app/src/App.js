0d6d92b01fd6b614e76f5a382cd5304428c6ef0a
NickMckenney <mckenney2001@gmail.com>
2019-12-05 18:37:31 -0500

    template setting up for porfolio. Used grid for styling

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./NavBar.js";
+import Projects from "./Projects.js";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Projects />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/NarBar.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./App.css";
+function NavBar() {
+  return <div className="NavBarContainer">HELLO</div>;
+}
+export default NavBar;
