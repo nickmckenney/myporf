e186e0fbd07e7384b7ecfdbf0378ef6e5fd7c9a9
NickMckenney <mckenney2001@gmail.com>
2020-01-10 17:05:08 -0500

    resized navbar and css stylinged

--- src/Projects.js
+++ src/Projects.js
@@ -5,6 +5,16 @@ import TowerOfHanoi from "./Tower.png";
 import calendar from "./calendar.png";
 import gov from "./gov.png";
 import Tripper from "./CoTripper.png";
+import { ReactComponent as Css } from "./css.svg";
+import { ReactComponent as Html } from "./html.svg";
+import { ReactComponent as Js } from "./js.svg";
+import { ReactComponent as ReactJs } from "./react.svg";
+import { ReactComponent as Wp } from "./wordpress.svg";
+import { ReactComponent as Python } from "./python.svg";
+import { ReactComponent as Ruby } from "./ruby.svg";
+import { ReactComponent as Node } from "./nodejs.svg";
+import { ReactComponent as Mongodb } from "./mongodb.svg";
+import { ReactComponent as Git } from "./git.svg";
 
 const FlippyStyle = {
   marginTop: "20%",
@@ -34,16 +44,27 @@ class App extends Component {
         <div className="ProjectsContainerAboutMe">
           <h1 className="projectDescriptionAbout">
             I recently graduated high school in 2019 and was driven to go
-            directly into a career I love. I was Previously a Google Data Center
+            directly into a career I love. I was previously a Google Data Center
             Technician repairing servers and learning things on the spot. After
             my position, I went directly to a coding Bootcamp.
           </h1>
         </div>
 
         <div className="ProjectContainerSkills">
-          <div className="projectDescriptionAbout">
+          <div className="projectDescriptionAbout projectPhone">
             <h1 className="currentSkills">Current Skills</h1>
-            <div className="pictureSkills"></div>
+            <div className="flex">
+              <Css />
+              <Html />
+              <Js />
+              <ReactJs />
+              <Wp />
+              <Python />
+              <Ruby />
+              <Node className="nodejs" />
+              <Mongodb />
+              <Git />
+            </div>
           </div>
         </div>
       </div>
